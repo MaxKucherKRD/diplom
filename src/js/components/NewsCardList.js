68692c205da632c74f63ea05a677b3597c6dc094
Maxim Kucherenko <kucherenko1648@yandex.ru>
2020-05-25 21:03:40 +0300

    fix error fetch

--- src/js/components/NewsCardList.js
+++ src/js/components/NewsCardList.js
@@ -1,14 +1,14 @@
 export default class NewsCardList {
-    constructor(NewsCard, NewsApi,calcTime) {
+    constructor(NewsCard, NewsApi, calcTime) {
         this.today = 0
         this.weekAgo = 6 // количество дней назад (отсчет с 0)
-        this.calcTime = calcTime;        
+        this.calcTime = calcTime;
         this.today = calcTime(this.today); // сегодняшняя дата
         this.calcDay = calcTime(this.weekAgo); // неделя назад 
-        this.NewsApi = (querry) =>  new NewsApi( querry,            
+        this.NewsApi = (querry) => new NewsApi(querry,
             `${this.today.getFullYear()}-${this.today.getMonth() + 1}-${this.today.getDate()}`,  // получаем дату в нужном формате ГГГГ-ММ-ДД
             `${this.calcDay.getFullYear()}-${this.calcDay.getMonth() + 1}-${this.calcDay.getDate()}`
-            )       
+        )
         this.createCard = (link, imageLink, date, title, description, author) => new NewsCard(link, imageLink, date, title, description, author)
         this.elementContainer = document.querySelector('.news__items');
         this.elementPreloader = document.querySelector('.news__load');
@@ -16,46 +16,55 @@ export default class NewsCardList {
         this.elementNews = document.querySelector('.news__container');
         this.elementError = document.querySelector('.news__error');
         this.buttonMore = document.querySelector('.news__button')
-        this.buttonMore.addEventListener('click', ()=>{this.showMore()});    
+        this.buttonMore.addEventListener('click', () => {
+            this.showMore()
+        });
     }
 
-    addCard(querry) {         
+    addCard(querry) {
         const api = this.NewsApi(querry)
         this._renderNews("load");
-        localStorage.clear();  
+        localStorage.clear();
         while (this.elementContainer.firstChild) {
-           this.elementContainer.removeChild(this.elementContainer.firstChild);
+            this.elementContainer.removeChild(this.elementContainer.firstChild);
         }
+
         api.getNews()
             .then(news => {
-                localStorage.setItem('querryResult',JSON.stringify(news))                
-                localStorage.setItem('querry',querry) 
+                localStorage.setItem('querryResult', JSON.stringify(news))
+                localStorage.setItem('querry', querry)
+
                 if (news.totalResults === 0) {
                     return Promise.reject("notFound")
                 }
                 if (news.totalResults <= 3) {
                     this.buttonMore.classList.add('news__button_hidden');
-                }                           
-                news.articles.forEach((item, index) => {   
-                                
+                }
+                news.articles.forEach((item, index) => {
+
                     const newCard = this.createCard(item.url, item.urlToImage, item.publishedAt, item.title, item.description, item.source.name);
-                    const {cardElement} = newCard;                                          
+                    const {cardElement} = newCard;
                     if (index > 2) {
                         cardElement.classList.add('news__item_hidden')// Рендерим только первые 3, остальные скрываем
-                    } 
+                    }
                     this.elementContainer.appendChild(cardElement);
                 })
             })
-            .then(() => {               
+            .then(() => {
+
                 this._renderNews("newsReady");
             })
-            .catch((status) => {               
+            .catch((status) => {
+                console.log(status)
+                status = 'TypeError: Failed to fetch' ? status = 'err' : status  ;
                 this._renderNews(status);
             })
-           
-         
+
+
     }
+
     _renderNews(status) {
+        debugger;
         switch (status) {
             case "load": // кейс 1 идет загрузка
                 this.elementPreloader.classList.remove('news__load_hidden')
@@ -75,28 +84,29 @@ export default class NewsCardList {
                 this.elementNotFound.classList.remove('news__not-found_hidden')
                 this.elementError.classList.add('news__error_hidden');
                 break;
-            case "err": // кейс 4 произошла ошибка на стороне сервера
+            case "err" : // кейс 4 произошла ошибка на стороне сервера
                 this.elementPreloader.classList.add('news__load_hidden')
                 this.elementNews.classList.add('news__container_hidden')
                 this.elementNotFound.classList.add('news__not-found_hidden')
                 this.elementError.classList.remove('news__error_hidden');
                 break;
+
         }
     }
-    showMore() {  
-       
-           
+
+    showMore() {
+
+
         this.items = document.querySelectorAll('.news__item_hidden')
         const cardLimit = 3
         for (let i = 0; i < cardLimit; i++) {  // Достаем 3 элемента
-           try {    // Если оставшихся элементов осталось меньше 3, пользователь не увидет ошибку.
-            this.items[i].classList.remove('news__item_hidden')            
-           }
-           catch {               
-           }     
+            try {    // Если оставшихся элементов осталось меньше 3, пользователь не увидет ошибку.
+                this.items[i].classList.remove('news__item_hidden')
+            } catch {
+            }
         }
         if (this.items.length <= cardLimit) { // скрываем кнопку, если нет скрытых карточек.
-            this.buttonMore.classList.add('news__button_hidden') 
+            this.buttonMore.classList.add('news__button_hidden')
         }
     }
 
